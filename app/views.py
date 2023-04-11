f9dda6c04ca864f60c5906a758fe2c5998eea54a
Nagaraju42 <nagarajukalngi@gmail.com>
2023-04-11 12:00:56 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Boy(request):
+    return render(request,'bootstrap_download.html')
---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Girl(request):
+    return render(request,'bootstrap.html')
